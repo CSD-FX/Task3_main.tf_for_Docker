4b7dafeed6f848e69aed1199a6fc8088ff795f95
CSD <apple@MacBookPro.lan>
2025-09-25 13:31:58 +0530

    Removed errors

--- EC2_Linux/main.tf
+++ EC2_Linux/main.tf
@@ -23,7 +23,7 @@ resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
   ports {
     internal = 80
-    external = 8080
+    external = 3000
   }
 }
 
--- MacOS/main.tf
+++ MacOS/main.tf
@@ -25,6 +25,6 @@ resource "docker_container" "nginx_container" {
 
   ports {
     internal = 80
-    external = 8080
+    external = 3000
   }
 }
